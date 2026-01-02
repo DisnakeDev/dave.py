ca89fd87ab429dd270d6a7d2d339621ff7b03a61
shiftinv <me@shiftinv.cc>
2026-01-02 17:53:03 +0000

    test: add pytest smoke test

---
+++ tests/test_basic.py
@@ -0,0 +1,7 @@
+import dave
+
+# The test setup is extremely simple, it primarily serves as a smoke test for CI for now.
+
+
+def test_max_version() -> None:
+    assert dave.get_max_supported_protocol_version() == 1
