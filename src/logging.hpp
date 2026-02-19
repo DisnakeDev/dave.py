3001e300c1c4c8fc3bb0a767f158211118ace4ae
shiftinv <me@shiftinv.cc>
2026-02-19 16:17:20 +0000

    feat: log `LS_INFO` logs as `logging.DEBUG` due to libdave verbosity

--- src/logging.hpp
+++ src/logging.hpp
@@ -9,13 +9,16 @@ namespace nb = nanobind;
 int map_logging_level(discord::dave::LoggingSeverity sev) {
     switch (sev) {
         case discord::dave::LoggingSeverity::LS_VERBOSE:
-            return 10;
+            return 10;  // logging.DEBUG
         case discord::dave::LoggingSeverity::LS_INFO:
-            return 20;
+            // n.b. this is not a typo.
+            // libdave's INFO logs are rather verbose (the LS_VERBOSE level is barely used),
+            // so just turn them into debug logs.
+            return 10;  // logging.DEBUG
         case discord::dave::LoggingSeverity::LS_WARNING:
-            return 30;
+            return 30;  // logging.WARNING
         case discord::dave::LoggingSeverity::LS_ERROR:
-            return 40;
+            return 40;  // logging.ERROR
         case discord::dave::LoggingSeverity::LS_NONE:
         default:
             return 0;
